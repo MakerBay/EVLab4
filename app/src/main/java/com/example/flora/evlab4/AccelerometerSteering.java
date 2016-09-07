cd28daa5315e3bf52ea865db5b5dda00a5b18ba2
Chris291 <christoph.bessler@tum.de>
2016-09-07 22:28:39 +0800

    Add files via upload
    updated orientation, locked to landscape - new axis for new Samsung 7" tablet, longClick incremental buttons

--- app/src/main/java/com/example/flora/evlab4/AccelerometerSteering.java
+++ app/src/main/java/com/example/flora/evlab4/AccelerometerSteering.java
@@ -6,6 +6,7 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -16,6 +17,7 @@ import android.os.Handler;
 import android.os.Vibrator;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -38,6 +40,12 @@ public class AccelerometerSteering extends Activity implements SensorEventListen
     BluetoothAdapter myBluetooth = null;
     BluetoothSocket btSocket = null;
     private boolean isBtConnected = false;
+
+    //Long Click Increment Handler
+    private Handler repeatUpdateHandler = new Handler();
+    private boolean brakeBoolean = false;
+    private boolean throttleBoolean = false;
+
 
     //Send data via Bluetooth
     String steeringAcc = String.valueOf(0);
@@ -70,10 +78,11 @@ public class AccelerometerSteering extends Activity implements SensorEventListen
 
     final static int middlePoint = 127;
     final static float rangeN = 127;
-    final static float rangeP = 128;
+    final static float rangeP = 127;
     final static float rangeSteering = 90;
 
-    Button btn_changeDirection, btn_motor, btn_disconnect;
+    Button btn_changeDirection, btn_motor, btn_disconnect, btn_throttle, btn_brake;
+
 
     private TextView angleZ, steeringValue, throttleValue, brakeValue;
 
@@ -83,7 +92,13 @@ public class AccelerometerSteering extends Activity implements SensorEventListen
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accelerometer_steering);
+
+        Intent newint = getIntent();
+        address = newint.getStringExtra(MainActivity.EXTRA_ADDRESSA);
+
         initializeViews();
+        //Connect to Bluetooth
+        new ConnectBT().execute();
         initializeButtons();
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
@@ -91,8 +106,11 @@ public class AccelerometerSteering extends Activity implements SensorEventListen
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
 
-        steeringAcc = String.valueOf(0);
-        throttleAcc = String.valueOf(0);
+        throttle = middlePoint;
+        brake = 0;
+
+        steeringAcc = String.valueOf(middlePoint);
+        throttleAcc = String.valueOf(middlePoint);
         brakeAcc = String.valueOf(0);
 
     }
@@ -137,6 +155,61 @@ public class AccelerometerSteering extends Activity implements SensorEventListen
                 disconnect();
             }
         });
+        btn_throttle.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                throttle++;
+                btn_throttle.setText(""+ throttle);
+            }
+        });
+
+        btn_throttle.setOnLongClickListener(
+                new View.OnLongClickListener(){
+                    public boolean onLongClick(View arg0){
+                        throttleBoolean = true;
+                        repeatUpdateHandler.post (new RptUpdater() );
+                        return false;
+                    }
+                }
+        );
+        btn_throttle.setOnTouchListener(new View.OnTouchListener(){
+            public boolean onTouch(View v, MotionEvent event){
+                if( (event.getAction() == MotionEvent.ACTION_UP || event.getAction() == MotionEvent.ACTION_CANCEL) && throttleBoolean ){
+                    throttleBoolean = false;
+                }
+                return false;
+            }
+        });
+
+
+        btn_brake.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                brake--;
+                btn_brake.setText("" + brake );
+            }
+        });
+
+        btn_brake.setOnLongClickListener(
+                new View.OnLongClickListener(){
+                    public boolean onLongClick(View arg0){
+                        brakeBoolean = true;
+                        repeatUpdateHandler.post (new RptUpdater() );
+                        return false;
+                    }
+                }
+        );
+
+        btn_brake.setOnTouchListener(new View.OnTouchListener(){
+            public boolean onTouch(View v, MotionEvent event){
+                if( (event.getAction() == MotionEvent.ACTION_UP || event.getAction() == MotionEvent.ACTION_CANCEL) && brakeBoolean ){
+                    brakeBoolean = false;
+                }
+                return false;
+            }
+        });
     }
 
     //onResume() register the accelerometer for listening the events
@@ -163,9 +236,13 @@ public class AccelerometerSteering extends Activity implements SensorEventListen
     @Override
     public void onSensorChanged(SensorEvent event) {
         Sensor sensor = event.sensor;
-        x = event.values[0];//always configure it according to device, the following code is tailored to a landscape tablet
-        y = event.values[1];//x-axis along long side (positive direction is left <-- right), y-axis along short side (90° CCW to x)
+        //x = event.values[0];//always configure it according to device, the following code is tailored to a landscape tablet
+        //y = event.values[1];//x-axis along long side (positive direction is left <-- right), y-axis along short side (90° CCW to x)
         //z-axis through display plane (positive direction is out of display)
+
+        //new tablet - x (short side, top to bottom, 90deg CW to y), y (long side towards home button), z (screen to backside)
+        x = -event.values[1];
+        y = event.values[0]; //this is only a quick fix - i suggest to convert the program to incorporate the different axis distribution of the new tablet
 
         ///call methods only when certain threshold in change is reached to save computing power?
 
@@ -174,18 +251,25 @@ public class AccelerometerSteering extends Activity implements SensorEventListen
 
         displayValues();
         addValues();
-        }
+    }
 
 
     void calcAngleZ_XY() { //calculate rotation around z-axis, based on gravitation along x and y
-        double xBaseP = 10.25;
-        double xBaseN = 9.35;
-        double yBaseP = 9.65;
+        double xBaseP = 9.75;
+        double xBaseN = 9.75;
+        double yBaseP = 9.70;
 
         double xPrec1 = 3.5;
         double xPrec2 = -3.8;
         double yPrec1 = 3.2;
         double yPrec2 = 3.5;
+
+        /*
+        Through the nature of the sine and cosine curves, it makes sense to either use only one axis or take the average of both
+        xPrec1/2 and yPrec1/2 are marking the borders, where to switch from x to (x+y)/2 and to y and back
+        These values have been obtained by testing and are roughly 20 degrees away from the turning points (70, 20, -20, -70)
+
+         */
 
         if (x > 0) {
             if (x < xPrec1) {
@@ -207,15 +291,15 @@ public class AccelerometerSteering extends Activity implements SensorEventListen
 
     void calcSteering() {
         if (angleZCalc > 0) {
-            int steeringLeft = Math.round(angleZCalc * rangeP / rangeSteering);
-            if (steeringLeft > rangeN) {
+            int steeringRight = Math.round(angleZCalc * rangeP / rangeSteering);
+            if (steeringRight > rangeN) {
                 steering = 0;
-            } else steering = middlePoint - steeringLeft;
-        } else if (angleZCalc < 0) {
-            int steeringRight = -Math.round(angleZCalc * rangeN / rangeSteering);
-            if (steeringRight > rangeP) {
-                steering = 255;
             } else steering = middlePoint + steeringRight;
+        } else if (angleZCalc < 0) {
+            int steeringLeft = -Math.round(angleZCalc * rangeN / rangeSteering);
+            if (steeringLeft > rangeP) {
+                steering = 255;
+            } else steering = middlePoint - steeringLeft;
         } else steering = middlePoint;
     }
 
@@ -235,11 +319,11 @@ public class AccelerometerSteering extends Activity implements SensorEventListen
 
     //addValues() to send data to Arduino via Bluetooth
     private void addValues() {
-        if (btSocket!=null) {
+        if (btSocket != null) {
             checksum_int = steering + throttle + brake;
             //checksum = String.valueOf(checksum_int);
 
-            send = steering+","+throttle+","+brake+","+checksum_int+"#";
+            send = steering + "," + throttle + "," + brake + "," + checksum_int + "#";
 
             try {
                 btSocket.getOutputStream().write(send.toString().getBytes());
@@ -252,7 +336,7 @@ public class AccelerometerSteering extends Activity implements SensorEventListen
 
 
     private void msg(String s) {
-        Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
     }
 
     @Override
@@ -283,46 +367,69 @@ public class AccelerometerSteering extends Activity implements SensorEventListen
         @Override
         protected Void doInBackground(Void... devices) {
             try {
-                if (btSocket == null || !isBtConnected)
-                {
+                if (btSocket == null || !isBtConnected) {
                     myBluetooth = BluetoothAdapter.getDefaultAdapter();
                     BluetoothDevice dispositivo = myBluetooth.getRemoteDevice(address);
                     btSocket = dispositivo.createInsecureRfcommSocketToServiceRecord(myUUID);
                     BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
                     btSocket.connect();
                 }
-            }
-            catch (IOException e) {
+            } catch (IOException e) {
                 ConnectSuccess = false;
             }
             return null;
         }
+
         @Override
         protected void onPostExecute(Void result) {
             super.onPostExecute(result);
 
             if (!ConnectSuccess) {
                 msg("Connection Failed. Is it a SPP Bluetooth? Try again.");
-                finish();
-            }
-            else {
+                //finish();
+            } else {
                 msg("Connected.");
                 isBtConnected = true;
             }
             progress.dismiss();
         }
     }
+
     //Disconnect() to close bluetooth socket
     private void disconnect() {
-        if (btSocket!=null) {
+        if (btSocket != null) {
             try {
                 btSocket.close();
+            } catch (IOException e) {
+                msg("Error");
             }
-            catch (IOException e)
-            { msg("Error");}
         }
         finish();
     }
 
 
+    class RptUpdater implements Runnable {
+
+        public void throttle() {
+            throttle++;
+            btn_throttle.setText("" + throttle);
+        }
+
+        public void brake() {
+            brake++;
+            btn_brake.setText("" + brake);
+        }
+
+        public void run() {
+            if (throttleBoolean) {
+                throttle();
+                repeatUpdateHandler.postDelayed(new RptUpdater(), 250);
+            } else if (brakeBoolean) {
+                brake();
+                repeatUpdateHandler.postDelayed(new RptUpdater(), 250);
+                // change later - exponential change of values
+            }
+
+        }
+    }
 }
